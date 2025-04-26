173266b15bd31b9bf510ed4fc4be5714ceb680e8
Panagiotis Varitis <panosvaritis@gmail.com>
2025-04-26 17:39:44 +0300

    Initial commit of the project

---
+++ src/main/java/dit/hua/compiler/Compiler.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dit.hua.compiler;
+
+/**
+ *
+ * @author panos-varitis
+ */
+public class Compiler {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
