43c1d6cc28994240654d3db111952e466afba526
Panagiotis Varitis <panosvaritis@gmail.com>
2025-06-28 10:49:37 +0300

    Added the program class for the ast. This class will represent the root of the ast

--- src/main/java/dit/hua/compiler/ast/Program.java
+++ src/main/java/dit/hua/compiler/ast/Program.java
@@ -1,5 +1,27 @@
 package dit.hua.compiler.ast;
 
-public class Program extends AST{
+import dit.hua.compiler.ast.definitions.FunctionDefinition;
 
+/*
+ * This is the root node of the AST. From there the tree expands dowards
+ * Matches program :: = func_def
+ */
+
+public class Program extends ASTNode{
+
+    private FunctionDefinition mainFunction;
+
+    public Program (FunctionDefinition mainFunction){
+        super();
+        this.mainFunction = mainFunction;
+    }
+
+    @Override
+    public void accept(ASTVisitor visitor) {
+        visitor.visit(this);        
+    }
+
+    public FunctionDefinition getMainFunction(){
+        return mainFunction;
+    }
 }
